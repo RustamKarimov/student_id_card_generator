cfa4f7a25b281144e4a14abf5081e2816ff234d1
Rustam Karimov <rustemk1981@gmail.com>
2020-05-20 23:27:08 +0200

    Data file path has been defined

--- packages/constants.py
+++ packages/constants.py
@@ -6,7 +6,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 TEMPLATE_FILE_NAME = 'student_card.pdf'
 TEMPLATE_FILE_PATH = os.path.join(TEMPLATES_DIR, TEMPLATE_FILE_NAME)
 
-DATA_FILE = ''
+DATA_DIR = os.path.join(BASE_DIR, 'data')
+DATA_FILE_NAME = 'student_list.xlsx'
+DATA_FILE_PATH = os.path.join(DATA_DIR, DATA_FILE_NAME)
 
 ID_FIELD = ''
 NAME_FIELD = ''
